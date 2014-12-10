00397b3b857f4278dd83a7d3049400604ebeb3bc
Michel <Michelgomesdes@hotmail.com>
2014-12-10 13:28:45 -0200

    Ajustando o rolamento do menu

--- script/menu_fixo.js
+++ script/menu_fixo.js
@@ -3,7 +3,7 @@
 			var offset = $('#meuMenu').offset().top;
 			var $meuMenu = $('#meuMenu');
 			$(document).on('scroll', function () {
-				if (300 <= $(window).scrollTop()) {
+				if (269 <= $(window).scrollTop()) {
 					$meuMenu.addClass('fixar');
 				} else {
 					$meuMenu.removeClass('fixar');
