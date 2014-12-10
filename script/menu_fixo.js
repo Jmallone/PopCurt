6c973c08b51c1a5cc632088b190c4cd9ce1db7f2
Michel <Michelgomesdes@hotmail.com>
2014-12-10 12:42:47 -0200

    Menu fixado ao rolar a pagina

---
+++ script/menu_fixo.js
@@ -0,0 +1,13 @@
+//<![CDATA[ 
+			$(window).load(function(){
+			var offset = $('#meuMenu').offset().top;
+			var $meuMenu = $('#meuMenu');
+			$(document).on('scroll', function () {
+				if (300 <= $(window).scrollTop()) {
+					$meuMenu.addClass('fixar');
+				} else {
+					$meuMenu.removeClass('fixar');
+				}
+			});
+
+			});//]]> 
